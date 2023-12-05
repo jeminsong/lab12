ac0c9191460df48a6cdcce96b03842f625ccc194
jeminsong <jeminsong0119@csu.fullerton.edu>
2023-12-05 14:15:12 -0800

    finished part-1.

--- part-1/animated_gradient_functions.cc
+++ part-1/animated_gradient_functions.cc
@@ -1,4 +1,7 @@
-// TODO: Add your header
+// Jemin Song
+// jeminsong0119@csu.fullerton.edu
+// @jeminsong
+// Partners: @Card1n
 
 #include "animated_gradient_functions.h"
 
@@ -7,32 +10,19 @@
 
 bool HasMatchingFileExtension(const std::string& file_name,
                               const std::string& extension) {
-  // TODO: Implement this function.
-  // Check if the length of the file_name is greater than
-  // or equal to the length of extension and compare if file_name ends
-  // with the extension using the compare member function, see
-  // https://en.cppreference.com/w/cpp/string/basic_string/compare
-  // Hint: Use file_name's compare member function:
-  //      file_name.compare()
-  // It will take three parameters.
-  // The first parameter is where you want to start comparing.
-  // Mathematically, this is the length of file_name minus the length
-  // of extension.
-  // The second parameter is how many letters you want to check
-  // which is the length of extension.
-  // The third parameter is what you want to compare file_name to,
-  // which is extension.
-  // Remember that file_name.compare() returns 0 if the comparison
-  // is true. This means you will want to use the 'equal to' operator
-  // '==' to check to see if file_name.compare() returned 0.
-  // ex. file_name.compare() == 0
+  if (file_name.length() >= extension.length()) {
+    return (file_name.compare(file_name.length() - extension.length(),
+                              extension.length(), extension) == 0);
+  }
   return false;
 }
 
 std::vector<double> BuildSineLookupTable(int image_width) {
   std::vector<double> lookup_table;
-  // TODO: build the lookup table and return it to the caller
-  // divide 180 degrees (M_PI) over number of columns or rows
-  // we work in radians because the math library works in radians.
+  double angle_step = M_PI / image_width;
+  for (int i = 0; i < image_width; ++i) {
+    double angle = angle_step * i;
+    lookup_table.push_back(std::sin(angle));
+  }
   return lookup_table;
 }
